a732a63408cc5d082aab3604cb858c29deb38b7b
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2023-10-02 16:10:03 -0300

    Add files via upload

---
+++ atv 12 q 9.c
@@ -0,0 +1,18 @@
+//atv 12 q 9
+#include <stdio.h>
+int main(){
+    int soma = 0;
+    int matriz[3][3] = {
+        {3,4,1},
+        {9,5,12},
+        {7,10,8}};
+    for (int row = 0;row<3;row++){
+        for (int column = 0;column<3;column++){
+            if (row > column){
+                soma = soma + matriz[row][column];
+                
+            }
+        }
+    }
+    printf("A soma dos termos acima da diagonal principal é %d",soma);
+}
